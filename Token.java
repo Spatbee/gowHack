182bd8a328c537e83f477a73f3d738f7cc9058fd
Daniel Burgess <frenchfrieman2@gmail.com>
2020-11-07 20:12:36 -0500

    updated token scores, updated notes

--- Token.java
+++ Token.java
@@ -1,10 +1,20 @@
 public enum Token {
-    COPPER,
-    SILVER,
-    GOLD,
-    BAG,
-    WOOD,
-    EMERALD,
-    RUBY,
-    SAFE;
+    COPPER(1),
+    SILVER(3),
+    GOLD(10),
+    BAG(30),
+    WOOD(80),
+    EMERALD(100),
+    RUBY(200),
+    SAFE(400);
+
+    private int score;
+
+    private Token(int score) {
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
 }
